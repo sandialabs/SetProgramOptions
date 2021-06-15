804c8de70fa4473f4b4ea3929d346519c561eb76
William McLendon <wcmclen@sandia.gov>
2021-06-15 11:31:23 -0600

    Refactor: Move setprogramoptions to src/
    - Changing up the gitlab-ci pipeline

--- exec-makedoc.sh
+++ exec-makedoc.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-python3 -m pip uninstall -y setprogramoptions
+# Source the common helpers script
+source scripts/common.bash
 
-./exec-reqs-install.sh
+printf "${yellow}"
+print_banner "Make Documentation - Started"
+printf "${normal}\n"
+
+execute_command "python3 -m pip uninstall -y setprogramoptions > _test-uninstall-spe.log 2>&1"
 
 cd doc
-./make_html_docs.sh
-err=$?
-exit $err
+execute_command_checked "./make_html_docs.sh"
+cd ..
 
+# only executes if the previous command didn't fail
+execute_command "rm _test-uninstall-spe.log > /dev/null 2>&1"
+
+printf "${yellow}"
+print_banner "Make Documentation - Done"
+printf "${normal}\n"
