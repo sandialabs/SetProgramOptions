ce48ef0143315bc321a412306b859743bcc034b3
William McLendon <wcmclen@sandia.gov>
2021-12-21 14:44:46 -0700

    cleaning up configuration files

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -57,7 +57,7 @@ from pathlib import Path
 import os
 import sys
 
-sys.path.append( str(Path.cwd().parent.parent.resolve().joinpath(Path("src")) ))
+sys.path.append(str(Path.cwd().parent.parent.resolve().joinpath(Path("src"))))
 
 # -- Project information -----------------------------------------------------
 
@@ -110,9 +110,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_css_files = [
-    'css/custom.css'
-]
+html_css_files = ['css/custom.css']
 
 
 
